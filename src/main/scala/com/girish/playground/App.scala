72e5f2140916f035c79e21ee6f5b7075dbcc0c8d
girish pasupathy <girishpasupathy@gmail.com>
2021-03-13 23:25:30 +0530

    Added scalafix and scalafmt support. Scalafix maven plugin is throwing some build failures.
    Changes to be committed:
    	new file:   .scalafix.conf
    	new file:   .scalafmt.conf
    	modified:   pom.xml
    	modified:   src/main/scala/com/girish/playground/App.scala

--- src/main/scala/com/girish/playground/App.scala
+++ src/main/scala/com/girish/playground/App.scala
@@ -1,8 +1,10 @@
 package com.girish.playground
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
  * @author ${girish}
  */
 object MainClass extends App {
-     //println( "Hello World!" )
+  //println( "Hello World!" )
 }
