d843e3944444235e13e8b83ba0b0c3fc8275481c
girish pasupathy <girishpasupathy@gmail.com>
2021-03-14 19:04:36 +0530

    Added logging using scala-logging and logback.
    Changes to be committed:
    	modified:   pom.xml
    	new file:   src/main/resources/logback.xml
    	modified:   src/main/scala/com/girish/playground/App.scala

--- src/main/scala/com/girish/playground/App.scala
+++ src/main/scala/com/girish/playground/App.scala
@@ -1,10 +1,25 @@
 package com.girish.playground
 
 import scala.collection.mutable.ArrayBuffer
+import com.typesafe.scalalogging._
 
 /**
  * @author girish
  */
 object MainClass extends App {
-  println("Hello World!")
+
+  val logger = Logger(MainClass.getClass)
+  /**
+   *
+   * @param name
+   */
+  def sayHello(name: String): Unit = {
+    logger.debug(s"Say hello to $name")
+    println(s"Hello, $name")
+    logger.info("Said hello to {}", name)
+  }
+
+  sayHello("John")
+  logger.trace(
+    "This log won't appear unless we have set the log level to accept trace in logback.xml")
 }
